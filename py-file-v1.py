02276517163cd16d9d7bd20130743efd661189c7
Andyzxc4 <lacraandre111@gmail.com>
2025-06-24 23:41:37 +0800

    feat: add test py file [py-file-v1.py]

---
+++ py-file-v1.py
@@ -0,0 +1,17 @@
+class Test:
+    def __init__(self):
+        pass
+
+    def test(self):
+        return "Your Turn"
+    
+    def test2(self):
+        return "New Test 2"
+    
+    def test3(self):
+        return "Not yet ready..."
+    
+test = Test()
+testOuput = test.test()
+
+print(testOuput)
